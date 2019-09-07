3b45dc2d67a5838b59d108676fcb62fde2ff3c65
Tyr Chen <tyr.chen@gmail.com>
2019-09-07 12:37:34 -0700

    use latest tool set

--- Makefile
+++ Makefile
@@ -1,3 +1,4 @@
+VERSION=$(strip $(shell cat version))
 TOP_DIR=.
 SRC_DIR=$(TOP_DIR)/slides
 TARGET_DIR=$(TOP_DIR)/_build
@@ -7,7 +8,7 @@ CLI_DIR=$(TOOL_DIR)/cli
 
 ENGINE=$(CLI_DIR)/engine.js
 
-MARP=marp --engine $(ENGINE) --theme $(ASSET_DIR)/custom.css --html true
+MARP=node_modules/@marp-team/marp-cli/marp-cli.js --engine $(ENGINE) --theme $(ASSET_DIR)/custom.css --html true
 
 init: install dep
 	@echo "Initializing the repo..."
@@ -17,7 +18,6 @@ travis-init: install dep
 
 install:
 	@echo "Install software required for this repo..."
-	@npm install -g @marp-team/marp-cli
 
 dep:
 	@echo "Install dependencies required for this repo..."
@@ -31,11 +31,17 @@ travis-deploy: release
 run:
 	@$(MARP) -s $(SRC_DIR)
 
-build: $(TARGET_DIR) copy-assets
-	@$(MARP) -I slides -o _build
+build: $(TARGET_DIR) gen-index
+	@$(MARP) -I slides -o $(TARGET_DIR)
 
 build-pdf: build
-	@$(MARP) -I slides -o _build --allow-local-files --pdf
+	@$(MARP) -I slides -o $(TARGET_DIR) --allow-local-files --pdf
+
+build-netlify: build
+	@mv $(SRC_DIR)/assets $(TARGET_DIR)
+
+link-assets:
+	@ln -s $(SRC_DIR)/assets $(TARGET_DIR)/assets || true
 
 copy-assets:
 	@rsync -arv $(SRC_DIR)/assets $(TARGET_DIR)
@@ -52,11 +58,36 @@ gen-meta:
 gen-src:
 	@cd resources; csplit -f '' -n 3 -k all.txt '/^#/' '{300}' || true; rename [0-9]* -a ".md"; mv *.md _src
 
+gen-index:
+	@tools/cli/gen_index.js -i resources/_meta -t index.md -o $(TARGET_DIR)/index.html
+
 $(TARGET_DIR):
 	@mkdir -p $@
 
 merge-audio: # need to define CHAPTER, N1 and N2, e.g. N1=23 N2=24, result will be 23_24.mp3
 	@cd slides/assets/audios/$(CHAPTER) && ffmpeg -i "concat:$(N1).mp3|$(N2).mp3" -acodec copy $(N1)_$(N2).mp3
+
+gen-all-audios:
+	num=1 ; while [[ $$num -le 9 ]] ; do \
+		make gen-audios CHAPTER=00$$num ; \
+		((num = num + 1)) ; \
+	done;
+	num=10 ; while [[ $$num -le 99 ]] ; do \
+		make gen-audios CHAPTER=0$$num ; \
+		((num = num + 1)) ; \
+	done;
+	num=100 ; while [[ $$num -le 294 ]] ; do \
+		make gen-audios CHAPTER=$$num ; \
+		((num = num + 1)) ; \
+	done
+
+
+
+
+correct-audio:
+	@cd slides/assets/audios/$(CHAPTER); rm $(N2).mp3;
+	@make gen-audios;
+	@make merge-audio
 
 split-audio: # need to define CHAPTER, N, SPLIT_AT, e.g. N=139, result will be 139_1.mp3, 139_2.mp3
 	@cd slides/assets/audios/$(CHAPTER) && ffmpeg -i ${N}.mp3 -acodec copy -ss 0 -to ${SPLIT_AT} ${N}_1.mp3 && ffmpeg -i ${N}.mp3 -acodec copy -ss ${SPLIT_AT} ${N}_2.mp3
--- tools/cli/normalizer.js
+++ tools/cli/normalizer.js
@@ -1,44 +1,43 @@
 #!/usr/bin/env node
-const fs = require('fs').promises
-const path = require('path')
+const fs = require('fs').promises;
+const path = require('path');
 
-const BREAKER = '\n\n'
-const MAX_BYTES = 1000
-const SENTENCE_BREAKER = '。'
+const BREAKER = '\n';
+const MAX_BYTES = 1000;
+const SENTENCE_BREAKER = '。';
 
 async function read(filename) {
-  console.log(`process ${filename}`)
-  const content = await fs.readFile(filename, 'utf8')
+  console.log(`process ${filename}`);
+  const content = await fs.readFile(filename, 'utf8');
 
   return content
     .split(BREAKER)
     .map(c => split_sentence(c, MAX_BYTES))
     .flat(100)
-    .map((c, idx) => normalize(c, idx))
+    .map((c, idx) => normalize(c, idx));
 }
 
 function split_sentence(text, max_len) {
-
-  if (Buffer.byteLength(text) <= max_len) return text
-  let idx = text.lastIndexOf(SENTENCE_BREAKER, 330)
-  if (idx < 0) console.log(text)
+  if (Buffer.byteLength(text) <= max_len) return text;
+  let idx = text.lastIndexOf(SENTENCE_BREAKER, 330);
+  if (idx < 0) console.log(text);
   if (text.substr(idx + 1, 1) === '”') idx = idx + 1;
-  return [text.substr(0, idx + 1), split_sentence(text.substr(idx + 1), max_len)]
+  return [text.substr(0, idx + 1), split_sentence(text.substr(idx + 1), max_len)];
 }
 
 function normalize(text, idx) {
-  if (text.startsWith('#')) return { text: text }
-  if (text.endsWith('）')) return { text: `## ${text}` }
-  if (text.length < 8 && /皇帝|王/.test(text)) return { text: `# ${text}` }
-  return { text: text, audio: `${idx}.mp3` }
+  if (text.startsWith('#')) return { text: text };
+  if (text.endsWith('）')) return { text: `## ${text}` };
+  if (text.length < 8 && /皇帝|王/.test(text)) return { text: `# ${text}` };
+  return { text: text, audio: `${idx}.mp3` };
 }
 
-async function process_file(in_file, out_file) {
+async function processFile(inFile, outFile) {
   try {
-    let data = await read(in_file)
-    await fs.writeFile(out_file, JSON.stringify(data, null, 2))
+    let data = await read(inFile);
+    await fs.writeFile(outFile, JSON.stringify(data, null, 2));
   } catch (e) {
-    console.log(e)
+    console.log(e);
   }
 }
 
@@ -53,20 +52,18 @@ async function main() {
       describe: 'output path to put generated json files'
     })
     .demandOption(['input', 'output'], 'Please provide both input path and output path')
-    .help()
-    .argv
+    .help().argv;
 
-
-  const files = await fs.readdir(argv.i)
+  const files = await fs.readdir(argv.i);
 
   results = files
     .filter(name => name.endsWith('.md'))
     .map(name => {
-      const in_file = path.join(argv.i, name)
-      const out_file = path.join(argv.o, `${path.basename(name, '.md')}.json`)
-      return process_file(in_file, out_file)
-    })
-  return await Promise.all(results)
+      const inFile = path.join(argv.i, name);
+      const outFile = path.join(argv.o, `${path.basename(name, '.md')}.json`);
+      return processFile(inFile, outFile);
+    });
+  return await Promise.all(results);
 }
 
-main()
+main();
